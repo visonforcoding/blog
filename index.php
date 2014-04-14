c8a7651ed2c51b1d1c6d5137ea540f2fee451ab6
allen <caowenpeng1990@126.com>
2014-04-14 23:41:56 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    define('APP_DEBUG',TRUE); // 开启调试模式
+    define('APP_PATH','./APP/');
+    define('APP_NAME', 'APP');
+   // define('APP_NAME', 'home');
+    require "ThinkPHP/ThinkPHP.php";
+	
